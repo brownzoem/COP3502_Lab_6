3620fb7dca37ef35e59162720b94bdd0b4c73269
brownzoem <brownzoem@gmail.com>
2023-10-24 10:26:36 -0400

    Lab 6 code

---
+++ main.py
@@ -0,0 +1,30 @@
+# This is the part 2 of Lab 6 if you are working individually
+# This is required to get full credit as a replacement for the partner
+
+# Instructions:
+# 1. Email me with your github email to be added as a collaborator
+# 2. Clone this repo
+# 3. Create a function that if your name is printed, the function asks the user a joke
+# 4. Be sure to make name the function with your name so that I can give you credit
+# 5. push your code back to the remote repository and ensure that it is added to the github
+
+# Example is shown below
+
+def zoe_brown():
+    user_response = input('What did the fish say when he swam into a wall?')
+    print('Dam')
+
+def main():
+    name = input("What is your name?")
+
+    if name == 'Zoe Brown':
+        zoe_brown()
+
+    # Add your code below:
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
