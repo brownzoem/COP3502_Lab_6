9c5f2891c430476550e124e3985d883b609136d0
Jose Ortega <joseortega200414@gmail.com>
2023-11-01 13:24:23 -0400

    Jose Ortega encode function

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def zoe_brown():
     user_response = input('What did the fish say when he swam into a wall?')
     print('Dam')
 
+#Jose Ortega encode function
+def encode_password(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+
 # Andy Hagen
 def decode(encoded_pass):
     # Create an empty string assigned to a new variable to store the decoded password
